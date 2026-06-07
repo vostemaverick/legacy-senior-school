8dac229c6be3c62f43151b3b6d3459438e8f04b6
Your Name <you@example.com>
2026-06-07 13:33:06 +0300

    Updated website

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDw6GFkOYOim3G_UtvpW8MecQANInnHnPs",
+  authDomain: "legacy-senior-school.firebaseapp.com",
+  projectId: "legacy-senior-school",
+  storageBucket: "legacy-senior-school.firebasestorage.app",
+  messagingSenderId: "363660115381",
+  appId: "1:363660115381:web:568dc3cff91dd7b5272dfc",
+  measurementId: "G-WX2NCHS0Q9"
+};
